d373066a993a4108b62754bd4c807fc64807b85b
Sean P. Myrick V19.0.3.1 <65933340+seanpm2001@users.noreply.github.com>
2020-07-22 17:00:04 -0700

    RuleList.py
    The rule list file for the software community government script.

---
+++ OpenGov/SoftwareCommunity_Government/Rules/RuleList.py
@@ -0,0 +1,38 @@
+# Start of script
+"""
+Software community government utility - Rule list
+"""
+communityNameMain = str("Untitled software community")
+communityRuleListInt = int(0)
+communityRuleListDec = float(0.0)
+communityRule1Title = str("Rule 1 title")
+communityRule1Desc = str("Rule 1 description")
+communityRule2Title = str("Rule 2 title")
+communityRule2Desc = str("Rule 2 description")
+communityRule3Title = str("Rule 3 title")
+communityRule3Desc = str("Rule 3 description")
+communityRule4Title = str("Rule 4 title")
+communityRule4Desc = str("Rule 4 description")
+communityRule5Title = str("Rule 5 title")
+communityRule5Desc = str("Rule 5 description")
+communityRule6Title = str("Rule 6 title")
+communityRule6Desc = str("Rule 6 description")
+communityRule7Title = str("Rule 7 title")
+communityRule7Desc = str("Rule 7 description")
+communityRule8Title = str("Rule 8 title")
+communityRule8Desc = str("Rule 8 description")
+communityRule9Title = str("Rule 9 title")
+communityRule9Desc = str("Rule 9 description")
+communityRule10Title = str("Rule 10 title")
+communityRule10Desc = str("Rule 10 description")
+print (str(communityNameMain())
+print("Rule list")
+# More of this program is coming soon
+noMorePleaseQuit = input("Press [ENTER] key to quit")
+print("If the program still isn't closed, please click the close button, or use a process manager and kill it manually if this doesn't work")
+"""
+File info
+File version: 1 (Wednesday, July 22nd 2020 at 5:00 pm)
+File type: Python script
+"""
+# End of script
